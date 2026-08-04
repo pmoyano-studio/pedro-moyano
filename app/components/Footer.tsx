1c5cc416326c0859bf1294b84b41fb8e36d96b0d
Pedro Moyano <info@pedromoyano.com>
2026-08-04 19:18:24 +0200

    footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,10 +2,9 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="w-full py-12 px-8 bg-white border-t border-neutral-100 text-neutral-500 text-xs font-light tracking-wider">
+    <footer className="w-full py-16 px-8 bg-white border-t border-neutral-200 text-neutral-600 text-sm font-normal tracking-wider">
       <div className="max-w-5xl mx-auto flex justify-between items-center">
         
-        {/* Solo el Copyright */}
         <p>© {currentYear} Pedro Moyano. All rights reserved.</p>
 
       </div>
